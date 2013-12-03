88bde4522712ac1643b95245ee456c38ec4b8be4
Ardian Haxha <ardian.haxha@gmail.com>
2013-12-03 21:51:49 +0100

    small fix to admin

--- rango/admin.py
+++ rango/admin.py
@@ -5,4 +5,4 @@ class PageAdmin(admin.ModelAdmin):
 	 list_display = ('title', 'category', 'url')
 
 admin.site.register(Category)
-admin.site.register(Page, Pageadmin)
+admin.site.register(Page, PageAdmin)
